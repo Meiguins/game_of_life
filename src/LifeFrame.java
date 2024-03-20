ca4117adcffc9df7680ab15ddf5e7c553495f12f
Meiguins <kauanmeiguins3@gmail.com>
2024-03-20 18:02:48 -0300

    Commit da Classe LifePanel

--- src/LifeFrame.java
+++ src/LifeFrame.java
@@ -4,6 +4,8 @@ public class LifeFrame extends JFrame {
     
     public LifeFrame(){
 
+        add(new LifePanel());
+
         setSize(1300,700);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
---
+++ src/LifePanel.java
@@ -0,0 +1,15 @@
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.*;
+
+public class LifePanel extends JPanel{
+    
+    public LifePanel(){
+        setSize(1300,700);
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+    }
+}
